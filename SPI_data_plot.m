f7e5c371aaddcec0db4dfe437672bd8413a73a16
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2024-08-16 22:00:44 -0700

    Add files via upload

--- SPI_data_plot.m
+++ SPI_data_plot.m
@@ -10,7 +10,7 @@ ECG_clean_table = readtable('ECG_clean.csv', 'VariableNamingRule', 'preserve');
 ecg_clean = zeros(1,a);
 ecg_clean = ECG_clean_table.Data;
 
-v = 3.3*(ecg_clean./4095);
+v = 3.3*(ecg_clean./4095); % change all to vref
 fs = 500;
 t = (0:length(v)-1)/(fs);
 figure('Color',[1,1,1]);
@@ -63,6 +63,63 @@ plot(t,v);
 title('ECG signal (After Jumping Jacks)');
 xlabel('Time (s)');
 ylabel('ECG Amplitude (V)');
+
+%%
+close all
+clc;
+
+% From electrode
+Vref = 3.12;
+Ts = 1/fs;
+n = 0:4095;
+t = n*Ts;
+
+ecg_body = 1.56 + 1.56*cos(2*pi*60*t) + 0.01*cos(2*pi*1*t) + 0.02*cos(2*pi*2*t);
+t = (0:length(ecg_body)-1)/(fs);
+
+ecg_body_seg = ecg_body(1250:2250); 
+ts = (0:length(ecg_body_seg)-1)/(fs);
+
+figure('Color',[1,1,1]);
+plot(ts,ecg_body_seg);
+title('ECG signal (At Electrode)');
+ylim([0 3.12]);
+xlabel('Time (s)');
+ylabel('ECG Amplitude (V)');
+
+
+% From AFE
+
+ecg_noisy_seg = Vref*(ecg_noisy(1250:2250)./4095); 
+ts = (0:length(ecg_noisy_seg)-1)/(fs);
+
+figure('Color',[1,1,1]);
+plot(ts,ecg_noisy_seg);
+title('ECG signal (AFE Output)');
+ylim([0 3.12]);
+xlabel('Time (s)');
+ylabel('ECG Amplitude (V)');
+
+% After Filter
+
+fc = 30;
+Wc = fc/(fs/2);
+b = fir1(50,Wc);
+ecg_filt1 = filtfilt(b,1,ecg_noisy);
+ecg_filt2 = smoothdata(ecg_filt1);
+ecg_filt_seg = Vref*(ecg_filt2(1250:2250)./4095); 
+ts = (0:length(ecg_filt_seg)-1)/(fs);
+
+figure('Color',[1,1,1]);
+plot(ts,ecg_filt_seg);
+title('ECG signal (Filtered)');
+ylim([0 3.12]);
+xlabel('Time (s)');
+ylabel('ECG Amplitude (V)');
+
+
+
+
 
 
 % % Code for 4096 samples @ fs = 20 KHz
