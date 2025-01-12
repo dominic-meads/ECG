07b62182e0e563a3fd6f167a40689a8b98c1cca6
dominic-meads <de.meads10@gmail.com>
2025-01-11 18:04:11 -0800

    create files

---
+++ SoC/Microblaze_AXIS_ECG_to_UART/Microblaze_AXIS_ECG_to_UART/Python/python_serial_read.py
@@ -0,0 +1,42 @@
+import serial
+import csv
+import os
+
+os.chdir('C:/Users/demea/ECG/SoC/Microblaze_AXIS_ECG_to_UART/Microblaze_AXIS_ECG_to_UART/Python')
+
+ser = serial.Serial(port='COM10',baudrate=115200)
+sample_number = 0
+
+fieldnames = ["value", "sample number"]
+
+with open('ECG_data.csv', 'w') as csv_file:
+    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    csv_writer.writeheader()
+
+# infinite loop except exits when Ctrl+C entered
+try:
+  while True:
+
+    with open('ECG_data.csv', 'a') as csv_file:
+      csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+      # get data from UART 
+      raw_serial_byte = ser.readline() # gets data in byte literal
+      value_byte_literal = raw_serial_byte.replace(b'\n', b'')  # remove newline
+      value_str = str(value_byte_literal,'UTF-8') # convert to string
+      value_int = int(value_str) # convert to int
+      print(value_int)
+
+      info = {
+          "value": value_int,
+          "sample number": sample_number
+      }
+
+      csv_writer.writerow(info)
+
+      sample_number += 1
+except KeyboardInterrupt:
+  pass
+
+print("Program halted")
+
---
+++ SoC/Microblaze_AXIS_ECG_to_UART/Microblaze_AXIS_ECG_to_UART/Python/python_serial_test_plot_2.py
@@ -0,0 +1,35 @@
+# NOTES: Run program in separate dedicated terminal
+# sources: https://www.youtube.com/watch?v=Ercd-Ip5PfQ&t=302s
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import os
+
+os.chdir('C:/Users/demea/ECG/SoC/Microblaze_AXIS_ECG_to_UART/Microblaze_AXIS_ECG_to_UART/Python')
+
+
+plt.style.use('fivethirtyeight')
+
+
+def animate_window(i):  # moving window of a segment of graph
+    data = pd.read_csv('ECG_data.csv')
+    x = data['sample number'][-1000:]
+    y = data['value'][-1000:]
+
+    plt.cla()
+
+    plt.plot(x, y, label='ECG Data')
+
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+
+try:
+  ani = FuncAnimation(plt.gcf(), animate_window, interval=10)
+
+  plt.tight_layout()
+  plt.show()
+except KeyboardInterrupt:
+  pass
+
+print("Program halted")
