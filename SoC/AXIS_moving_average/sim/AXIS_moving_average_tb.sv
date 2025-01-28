022257a07e360edb1e41e5f6c6dac047375d2d1b
dominic-meads <de.meads10@gmail.com>
2025-01-27 23:05:37 -0800

    create files

---
+++ SoC/AXIS_moving_average/sim/AXIS_moving_average_tb.sv
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+
+module AXIS_moving_average_tb;
+  reg  clk;
+  reg  rst_n;
+  reg  s_axis_tvalid;
+  reg  m_axis_tready; 
+  reg  signed [31:0] s_axis_tdata;
+  wire signed [31:0] m_axis_tdata;
+  wire m_axis_tvalid;
+  wire s_axis_tready;
+
+  // 50 MHz clock
+  always #10 clk = ~clk; 
+
+  AXIS_moving_average_tb uut (
+    .clk(clk),                      // input wire aclk
+    .s_axis_tvalid(s_axis_tvalid),  // input wire s_axis_data_tvalid
+    .s_axis_tready(s_axis_tready),  // output wire s_axis_data_tready
+    .s_axis_tdata(s_axis_tdata),    // input wire [31 : 0] s_axis_data_tdata
+    .m_axis_tvalid(m_axis_tvalid),  // output wire m_axis_data_tvalid
+    .m_axis_tdata(m_axis_tdata)     // output wire [31 : 0] m_axis_data_tdata
+  );
+
+   initial 
+    begin
+      clk = 1'b0;
+      rst_n = 1'b0; 
+      s_axis_tdata = 0;
+      s_axis_tvalid = 1'b0;
+      m_axis_tready = 1'b1; // upstream device ready
+      #40
+      rst_n = 1'b1; // release reset
+      
+      // output a signal that alternates between the value 3 and 5 (average should be 4 after initial delay of 74 samples)
+      repeat(50)  // 500 Hz sampling
+        begin 
+          #1980
+          s_axis_tdata = 5;
+          wait (clk == 1'b0) wait (clk == 1'b1) // wait for rising edge of clock
+          s_axis_tvalid = 1'b1;
+          #20
+          s_axis_tvalid = 1'b0; // tvalid only high for 1 clock cycle
+
+          #1980
+          s_axis_tdata = 3;
+          wait (clk == 1'b0) wait (clk == 1'b1) // wait for rising edge of clock
+          s_axis_tvalid = 1'b1;
+          #20
+          s_axis_tvalid = 1'b0; // tvalid only high for 1 clock cycle
+        end
+        #50000
+        $finish;
+      $finish;
+    end
+
+endmodule
---
+++ SoC/AXIS_moving_average/src/AXIS_moving_average.v
@@ -0,0 +1,45 @@
+  `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Dominic Meads
+// 
+// Create Date: 1/27/2025 10:05:43 PM
+// Design Name: 
+// Module Name: FIR_bp
+// Project Name: 
+// Target Devices: 7 Series
+// Tool Versions: Vivado 2024.1
+// Description: 
+//      74-point moving average filter generated with a FIR compiler (all taps have same coefficient) 
+//
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//
+//////////////////////////////////////////////////////////////////////////////////  
+module AXIS_moving_average (
+  input  clk,
+  input  s_axis_tvalid,
+  input  signed [31:0] s_axis_tdata,
+  output signed [31:0] m_axis_tdata,
+  output m_axis_tvalid,
+  output s_axis_tready
+  ); 
+  
+  wire signed [39:0] w_m_axis_tdata;
+  
+  fir_compiler_0 fir0 (
+    .aclk(clk),                          // input wire aclk
+    .s_axis_data_tvalid(s_axis_tvalid),  // input wire s_axis_data_tvalid
+    .s_axis_data_tready(s_axis_tready),  // output wire s_axis_data_tready
+    .s_axis_data_tdata(s_axis_tdata),    // input wire [15 : 0] s_axis_data_tdata
+    .m_axis_data_tvalid(m_axis_tvalid),  // output wire m_axis_data_tvalid
+    .m_axis_data_tdata(w_m_axis_tdata)     // output wire [39 : 0] m_axis_data_tdata
+  );
+  
+  assign m_axis_tdata = w_m_axis_tdata >>> 23;
+
+endmodule
