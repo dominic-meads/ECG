0543779cfa2d53f8ac7bbf4f6a5ffdbe9145c449
dominic-meads <de.meads10@gmail.com>
2025-05-06 22:52:40 -0700

    converted sample integers to arrays, changed code to use arrays

--- SoC/QRS_detection/vitis/hello_world/src/helloworld.c
+++ SoC/QRS_detection/vitis/hello_world/src/helloworld.c
@@ -23,8 +23,10 @@ Dominic Meads
 #include "mb_interface.h"
 #include "xparameters.h"
 
-#define DEBUG_60_BPM
-//#define DEBUG_40_BPM
+//#define DEBUG_60_BPM
+#define DEBUG_40_BPM
+
+#define SAMPLE_VECTOR_SIZE 9
 
 #ifdef DEBUG_60_BPM
     #define DERIV_TO_MA_DELAY_CYCLES 18
@@ -112,138 +114,76 @@ int impulsive_max_has_occurred(int past_4_sample, int past_2_sample, int current
     return status;
 }
 
+// function to right shift array by 1 element
+void right_shift_array(int * arr, int size)
+{
+    for(int i = size-1; i > 0; i--)
+    {
+        arr[i] = arr[i-1];
+    }
+    arr[0] = 0;
+}
+
 int main()
 {
     init_platform();
     microblaze_disable_interrupts();
 
-    int past_8_ch0_sample  = 0; // the oldest sample (delay of 8)
-    int past_7_ch0_sample  = 0; // past (delay of 6)
-    int past_6_ch0_sample  = 0;  
-    int past_5_ch0_sample  = 0;  
-    int past_4_ch0_sample  = 0;  
-    int past_3_ch0_sample  = 0;  
-    int past_2_ch0_sample  = 0;  
-    int past_ch0_sample    = 0; // the previous sample
-    int current_ch0_sample = 0; // current sample
-
-    int past_8_ch1_sample  = 0; 
-    int past_7_ch1_sample  = 0;
-    int past_6_ch1_sample  = 0; 
-    int past_5_ch1_sample  = 0;  
-    int past_4_ch1_sample  = 0; 
-    int past_3_ch1_sample  = 0; 
-    int past_2_ch1_sample  = 0;  
-    int past_ch1_sample    = 0;  
-    int current_ch1_sample = 0; 
-
-    int past_8_ch2_sample  = 0; 
-    int past_7_ch2_sample  = 0;
-    int past_6_ch2_sample  = 0; 
-    int past_5_ch2_sample  = 0;  
-    int past_4_ch2_sample  = 0; 
-    int past_3_ch2_sample  = 0;    
-    int past_2_ch2_sample  = 0;  
-    int past_ch2_sample    = 0;  
-    int current_ch2_sample = 0; 
+    // arrays to hold samples, index 19 is the oldest sample, index 0 is the newest
+    int ch0_samples[SAMPLE_VECTOR_SIZE] = {};
+    int ch1_samples[SAMPLE_VECTOR_SIZE] = {};
+    int ch2_samples[SAMPLE_VECTOR_SIZE] = {};
 
     // sample 2nd derivative (ch2) DERIV_TO_MA_DELAY_CYCLES times before sampling moving average to align
     for(int i = 0; i < DERIV_TO_MA_DELAY_CYCLES; i++)
     {
-        getfsl(current_ch2_sample, 2);  
+        getfsl(ch2_samples[0], 2);  
     }
 
     // sample Moving Averager (ch1) AND 2nd derivative (ch2) MA_TO_FIR_DELAY_CYCLES times before sampling FIR BP
     for(int i = 0; i < MA_TO_FIR_DELAY_CYCLES; i++)
     {
-        getfsl(current_ch1_sample, 1); 
-        getfsl(current_ch2_sample, 2);  
+        getfsl(ch1_samples[0], 1); 
+        getfsl(ch2_samples[0], 2);  
     }
 
     // enter infinite loop now that all signals are aligned
     while(1)
     {
-        // update past samples
-        past_8_ch0_sample = past_7_ch0_sample;
-        past_7_ch0_sample = past_6_ch0_sample;
-        past_6_ch0_sample = past_5_ch0_sample; 
-        past_5_ch0_sample = past_4_ch0_sample;  
-        past_4_ch0_sample = past_3_ch0_sample;
-        past_3_ch0_sample = past_2_ch0_sample;
-        past_2_ch0_sample = past_ch0_sample;
-        past_ch0_sample = current_ch0_sample;
-
-        past_8_ch1_sample = past_7_ch1_sample;
-        past_7_ch1_sample = past_6_ch1_sample;
-        past_6_ch1_sample = past_5_ch1_sample; 
-        past_5_ch1_sample = past_4_ch1_sample;
-        past_4_ch1_sample = past_3_ch1_sample;
-        past_3_ch1_sample = past_2_ch1_sample;
-        past_2_ch1_sample = past_ch1_sample;
-        past_ch1_sample = current_ch1_sample;
-
-        past_8_ch2_sample = past_7_ch2_sample;
-        past_7_ch2_sample = past_6_ch2_sample;
-        past_6_ch2_sample = past_5_ch2_sample; 
-        past_5_ch2_sample = past_4_ch2_sample;
-        past_4_ch2_sample = past_3_ch2_sample;
-        past_3_ch2_sample = past_2_ch2_sample;
-        past_2_ch2_sample = past_ch2_sample;
-        past_ch2_sample = current_ch2_sample;        
+        // shift to make room for new sample
+        right_shift_array(ch0_samples, SAMPLE_VECTOR_SIZE);
+        right_shift_array(ch1_samples, SAMPLE_VECTOR_SIZE);
+        right_shift_array(ch2_samples, SAMPLE_VECTOR_SIZE);      
 
         // get current sample for all channels
-        getfsl(current_ch0_sample, 0);
-        getfsl(current_ch1_sample, 1); 
-        getfsl(current_ch2_sample, 2);
+        getfsl(ch0_samples[0], 0);
+        getfsl(ch1_samples[0], 1); 
+        getfsl(ch2_samples[0], 2); 
 
         // start looking for max of 2nd derivative (ch2)
-        if (flat_max_has_occurred(past_8_ch2_sample, past_4_ch2_sample, current_ch2_sample, DERIV_THRESHOLD_VALUE) == 1) 
+        if (flat_max_has_occurred(ch2_samples[8], ch2_samples[4], ch2_samples[0], DERIV_THRESHOLD_VALUE) == 1) 
         {
             //xil_printf("max of 2nd deriv occured----------\n\r");
-            if (past_4_ch2_sample >= DERIV_THRESHOLD_VALUE)  // max must be greater than threshold
+            if (ch2_samples[4] >= DERIV_THRESHOLD_VALUE)  // max must be greater than threshold
             {
-                // look for max of ECG/FIR BP (ch0) until max (above threshold) of moving average is found
-                //while(flat_max_has_occurred(past_8_ch1_sample, past_4_ch1_sample, current_ch1_sample) == 0 && past_4_ch1_sample > MA_THRESHOLD_VALUE)
-                while(flat_max_has_occurred(past_8_ch1_sample, past_4_ch1_sample, current_ch1_sample, MA_THRESHOLD_VALUE) == 0) 
+                // look for max of ECG/FIR BP (ch0) until max (above threshold) of moving average (ch1) is found
+                while(flat_max_has_occurred(ch1_samples[8], ch1_samples[4], ch1_samples[0], MA_THRESHOLD_VALUE) == 0) 
                 {
-                    // update past samples
-                    past_8_ch0_sample = past_7_ch0_sample;
-                    past_7_ch0_sample = past_6_ch0_sample;
-                    past_6_ch0_sample = past_5_ch0_sample; 
-                    past_5_ch0_sample = past_4_ch0_sample;  
-                    past_4_ch0_sample = past_3_ch0_sample;
-                    past_3_ch0_sample = past_2_ch0_sample;
-                    past_2_ch0_sample = past_ch0_sample;
-                    past_ch0_sample = current_ch0_sample;
-
-                    past_8_ch1_sample = past_7_ch1_sample;
-                    past_7_ch1_sample = past_6_ch1_sample;
-                    past_6_ch1_sample = past_5_ch1_sample; 
-                    past_5_ch1_sample = past_4_ch1_sample;
-                    past_4_ch1_sample = past_3_ch1_sample;
-                    past_3_ch1_sample = past_2_ch1_sample;
-                    past_2_ch1_sample = past_ch1_sample;
-                    past_ch1_sample = current_ch1_sample;
-
-                    past_8_ch2_sample = past_7_ch2_sample;
-                    past_7_ch2_sample = past_6_ch2_sample;
-                    past_6_ch2_sample = past_5_ch2_sample; 
-                    past_5_ch2_sample = past_4_ch2_sample;
-                    past_4_ch2_sample = past_3_ch2_sample;
-                    past_3_ch2_sample = past_2_ch2_sample;
-                    past_2_ch2_sample = past_ch2_sample;
-                    past_ch2_sample = current_ch2_sample;   
+                    // shift to make room for new sample
+                    right_shift_array(ch0_samples, SAMPLE_VECTOR_SIZE);
+                    right_shift_array(ch1_samples, SAMPLE_VECTOR_SIZE);
+                    right_shift_array(ch2_samples, SAMPLE_VECTOR_SIZE);      
 
                     // get current sample for all channels
-                    getfsl(current_ch0_sample, 0);
-                    getfsl(current_ch1_sample, 1); 
-                    getfsl(current_ch2_sample, 2); 
+                    getfsl(ch0_samples[0], 0);
+                    getfsl(ch1_samples[0], 1); 
+                    getfsl(ch2_samples[0], 2);
 
                     // detect max above specified threshold
                     // look for max over smaller window for max (qrs complex more implusive than MA or 2nd deriv)
-                    if(impulsive_max_has_occurred(past_4_ch0_sample, past_2_ch0_sample, current_ch0_sample, FIR_BP_THRESHOLD_VALUE) == 1)
+                    if(impulsive_max_has_occurred(ch0_samples[4], ch0_samples[2], ch0_samples[0], FIR_BP_THRESHOLD_VALUE) == 1)
                     {
-                        xil_printf("%d,%d,%d, 1\n\r",current_ch0_sample,current_ch1_sample,current_ch2_sample);  // print a 1 to show peak occurs here
+                        xil_printf("%d,%d,%d, 1\n\r",ch0_samples[0],ch1_samples[0],ch2_samples[0]);  // print a 1 to show peak occurs here
                         // what I think is happening here is that because I am using 5 samples in my 
                         // max_has_occured() function, there are essentially two maxes occuring next
                         // to each other. Maybe implement some sort of delay after the first one has occured?
@@ -252,19 +192,19 @@ int main()
                     }
                     else
                     {
-                        xil_printf("%d,%d,%d, 0\n\r",current_ch0_sample,current_ch1_sample,current_ch2_sample);  // print a 0 to show no peak at current sample
+                        xil_printf("%d,%d,%d, 0\n\r",ch0_samples[0],ch1_samples[0],ch2_samples[0]);  // print a 0 to show no peak at current sample
                     }
                 }
                 //xil_printf("MA max occured--------\n\r");
             }
             else
             {
-                xil_printf("%d,%d,%d, 0\n\r",current_ch0_sample,current_ch1_sample,current_ch2_sample);  // print a 0 to show no peak at current sample
+                xil_printf("%d,%d,%d, 0\n\r",ch0_samples[0],ch1_samples[0],ch2_samples[0]);  // print a 0 to show no peak at current sample
             }
         }
         else 
         {
-            xil_printf("%d,%d,%d, 0\n\r",current_ch0_sample,current_ch1_sample,current_ch2_sample);  // print a 0 to show no peak at current sample
+            xil_printf("%d,%d,%d, 0\n\r",ch0_samples[0],ch1_samples[0],ch2_samples[0]);  // print a 0 to show no peak at current sample
         }   
     }
 
